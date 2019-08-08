4d63e62adbae3ab6e29eccf2ae092367b9724543
unknown <865553742@qq.com>
2019-08-09 00:17:35 +0800

    feat: init

---
+++ app/controller/users.js
@@ -0,0 +1,84 @@
+const Controller = require('egg').Controller;
+
+function toInt(str) {
+  if (typeof str === 'number') return str;
+  if (!str) return str;
+  return parseInt(str, 10) || 0;
+}
+
+
+/**
+ * @Controller user
+ */
+class UserController extends Controller {
+  /**
+   * @Router GET /users
+   * @Description 请求 user 列表
+   */
+  async index() {
+    const ctx = this.ctx;
+    const query = {
+      limit: toInt(ctx.query.limit),
+      offset: toInt(ctx.query.offset)
+    };
+    ctx.body = await ctx.model.User.findAll(query);
+  }
+
+  /**
+   * @Router GET /user/:id
+   * @Description 请求单个 user
+   */
+  async show() {
+    const ctx = this.ctx;
+    ctx.body = await ctx.model.User.findByPk(toInt(ctx.params.id));
+  }
+
+  /**
+   * @Router POST /users
+   * @Description 创建 user
+   */
+  async create() {
+    const ctx = this.ctx;
+    const { name, age } = ctx.request.body;
+    const user = await ctx.model.User.create({ name, age });
+    ctx.status = 201;
+    ctx.body = user;
+  }
+
+  /**
+   * @Router PUT /users/:id
+   * @Description 修改 user
+   */
+  async update() {
+    const ctx = this.ctx;
+    const id = toInt(ctx.params.id);
+    const user = await ctx.model.User.findByPk(id);
+    if (!user) {
+      ctx.status = 404;
+      return;
+    }
+
+    const { name, age } = ctx.request.body;
+    await user.update({ name, age });
+    ctx.body = user;
+  }
+
+  /**
+   * @Router DELETE /users/:id
+   * @Description 删除 user
+   */
+  async destroy() {
+    const ctx = this.ctx;
+    const id = toInt(ctx.params.id);
+    const user = await ctx.model.User.findByPk(id);
+    if (!user) {
+      ctx.status = 404;
+      return;
+    }
+
+    await user.destroy();
+    ctx.status = 200;
+  }
+}
+
+module.exports = UserController;
